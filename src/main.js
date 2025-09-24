c0c5c5c40f775e464385a2e7ec78c4b8af0c3ea4
sKirillDev <k.sychov.dp@gmail.com>
2025-09-24 10:17:35 +0300

    feat: about section done

--- src/main.js
+++ src/main.js
@@ -13,16 +13,16 @@ const social = collapse ? collapse.querySelector('.navbar__social') : null;
 const BREAKPOINT = 992;
 
 function moveElements() {
-    if (!dropdown || !line || !collapse || !navExtra) return;
+  if (!dropdown || !line || !collapse || !navExtra) return;
 
-    if (window.innerWidth < BREAKPOINT) {
-        if (!navExtra.contains(dropdown)) navExtra.appendChild(dropdown);
-        if (!navExtra.contains(line)) navExtra.appendChild(line);
-    } else {
-        const refNode = social || null;
-        if (!collapse.contains(dropdown)) collapse.insertBefore(dropdown, refNode);
-        if (!collapse.contains(line)) collapse.insertBefore(line, refNode);
-    }
+  if (window.innerWidth < BREAKPOINT) {
+    if (!navExtra.contains(dropdown)) navExtra.appendChild(dropdown);
+    if (!navExtra.contains(line)) navExtra.appendChild(line);
+  } else {
+    const refNode = social || null;
+    if (!collapse.contains(dropdown)) collapse.insertBefore(dropdown, refNode);
+    if (!collapse.contains(line)) collapse.insertBefore(line, refNode);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', moveElements);
@@ -35,3 +35,26 @@ navToggle.addEventListener('click', () => firstScreen.classList.toggle('active')
 
 
 
+const swiper = new Swiper(".swiper", {
+  effect: "cards",
+  grabCursor: true,
+  cardsEffect: {
+    perSlideRotate: 0,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  on: {
+      reachEnd: function () {
+        setTimeout(() => {
+          this.slideTo(0, 500);
+        }, 300);
+      },
+    },
+});
