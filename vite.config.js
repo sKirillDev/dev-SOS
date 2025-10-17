2ba790e04bf2c4cde6a5a751a536f215ff991eb5
sKirillDev <k.sychov.dp@gmail.com>
2025-10-17 19:03:54 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import { resolve } from 'path'
 import FullReload from 'vite-plugin-full-reload'
 
 export default defineConfig({
+  base: '/dev-SOS/',
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "src/partials")
